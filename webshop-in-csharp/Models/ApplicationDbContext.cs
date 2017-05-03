a7796c6283eea2a957a736138b19e6acc82e552a
JacketWhite <feherkeee@gmail.com>
2017-05-03 17:05:26 +0200

    Empty user Model

---
+++ webshop-in-csharp/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebshopInCsharp.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebshopInCsharp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+
+    }
+}
