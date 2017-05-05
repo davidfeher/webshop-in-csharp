e8e993528b361140bbee1f088b0b311b057f624c
JacketWhite <feherkeee@gmail.com>
2017-05-06 00:57:45 +0200

    DbContext and User entity update

--- webshop-in-csharp/Models/ApplicationDbContext.cs
+++ webshop-in-csharp/Models/ApplicationDbContext.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using WebshopInCsharp.Models.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebshopInCsharp.Models;
 
 namespace WebshopInCsharp.Models
 {
     public class ApplicationDbContext : IdentityDbContext<User>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Profile> Profiles { get; set; }
+
+        public DbSet<ProductCategory> Category { get; set; }
+
+        public DbSet<Supplier> Supplier { get; set; }
     }
 }
